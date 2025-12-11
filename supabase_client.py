5d9df08eaab809151feeff774ce5c9b5edc27443
4ra-B <laura.benkel@matchi.com>
2025-12-11 09:15:17 +0100

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,7 +1,7 @@
-from supabase import create_client
-import streamlit as st
+from dotenv import load_dotenv
+import os
 
-def get_supabase():
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
-    return create_client(url, key)
+load_dotenv()
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
