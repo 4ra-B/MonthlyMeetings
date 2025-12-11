4f56f227471ec9382f35efa98589f674917346f1
4ra-B <laura.benkel@matchi.com>
2025-12-11 09:24:29 +0100

    Implement get_supabase function
    Add function to create Supabase client using Streamlit secrets

--- supabase_client.py
+++ supabase_client.py
@@ -1,7 +1,13 @@
 from dotenv import load_dotenv
 import os
+from supabase import create_client
+import streamlit as st
 
 load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
+def get_supabase():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
